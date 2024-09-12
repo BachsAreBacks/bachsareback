d300cf54c024815a3168ca8526d2acd1aaaa7498
BachsAreBacks <iloveDJMie@gmail.com>
2024-09-12 18:03:43 +0700

    commons

---
+++ env.php
@@ -0,0 +1,15 @@
+<?php 
+
+// Biến môi trường, dùng chung toàn hệ thống
+// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
+
+define('BASE_URL'       , 'http://localhost/P_Manager/');
+define('BASE_URL_ADMIN' , 'http://localhost/P_Manager/admin/');
+
+define('DB_HOST'    , 'localhost');
+define('DB_PORT'    , 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME'    , 'manager');
+
+define('PATH_ROOT'    , __DIR__ . '/../');
