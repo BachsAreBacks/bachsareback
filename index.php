b35b31ef842a023314777a13052689ebe0da9d55
BachsAreBacks <iloveDJMie@gmail.com>
2024-09-12 18:12:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
-<?php 
-// Common
-require_once './commons/env.php'; // Khai báo biến môi trường
-require_once './commons/function.php'; // Hàm hỗ trợ
+// <?php 
+// // Common
+// require_once './commons/env.php'; // Khai báo biến môi trường
+// require_once './commons/function.php'; // Hàm hỗ trợ
 
-// Controllers
-require_once "./controllers/UserController.php";
+// // Controllers
+// require_once "./controllers/UserController.php";
 
-// Models
-require_once "./models/UserModel.php";
+// // Models
+// require_once "./models/UserModel.php";
 
-// Route
-$act = $_GET['act'] ?? '/';
+// // Route
+// $act = $_GET['act'] ?? '/';
 
-// Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
+// // Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
 
-match ($act) {
-    // Trang chủ
-    '/' => (new UserController()) -> home_view(),
+// match ($act) {
+//     // Trang chủ
+//     '/' => (new UserController()) -> home_view(),
 
-};
+// };
+
+<?php echo "Bach" ?>
